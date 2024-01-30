1874ccf4675510d34b9b0faaf50c06989996c876
Anshuman Sharma <anshuman9998@gmail.com>
2024-01-31 01:30:14 +0530

    home screen

---
+++ lib/src/constants.dart
@@ -0,0 +1,12 @@
+import 'dart:ui';
+
+class AppColors {
+  static const appBackgroundColor = Color(0xFF000000);
+  static const appActiveColor = Color(0xFFFFFFFF);
+  static const greenDark = Color(0xFF34A853);
+  static const graphLine = Color(0xFF0A7A28);
+  static const redColor = Color(0xFF530500);
+  static const goldColor = Color(0xFFFFD662);
+  static const inputFieldGradient = [appBackgroundColor, appActiveColor];
+  static const dividerColor = Color.fromARGB(255, 73, 72, 72);
+}
---
+++ lib/src/wish/presentation/Screens/Home_Screen.dart
@@ -0,0 +1,162 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:wish/src/constants.dart';
+import 'package:wish/src/wish/presentation/utils/dotted_line.dart';
+
+class HomeScreen extends ConsumerStatefulWidget {
+  const HomeScreen({super.key});
+
+  @override
+  ConsumerState<ConsumerStatefulWidget> createState() => _HomeScreenState();
+}
+
+class _HomeScreenState extends ConsumerState<HomeScreen> {
+  int _selectedIndex = 0;
+  static const TextStyle optionStyle =
+      TextStyle(fontSize: 30, fontWeight: FontWeight.bold);
+  static const List<Widget> _widgetOptions = <Widget>[
+    Text(
+      'Index 0: Home',
+      style: optionStyle,
+    ),
+    Text(
+      'Index 1: Business',
+      style: optionStyle,
+    ),
+    Text(
+      'Index 2: School',
+      style: optionStyle,
+    ),
+  ];
+
+  void _onItemTapped(int index) {
+    setState(() {
+      _selectedIndex = index;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: AppColors.appBackgroundColor,
+      drawer: Drawer(
+        width: 250,
+        backgroundColor: AppColors.appBackgroundColor,
+        child: ListView(
+          children: [
+            const SizedBox(
+              height: 27,
+            ),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.end,
+              children: [
+                IconButton(
+                    onPressed: () {
+                      Navigator.pop(context);
+                    },
+                    icon: const Icon(
+                      Icons.arrow_back_ios,
+                      color: AppColors.appActiveColor,
+                    ))
+              ],
+            ),
+            const Divider(
+              thickness: 0.7,
+              color: AppColors.dividerColor,
+            ),
+            ListTile(
+              title: const Row(
+                children: [
+                  Icon(
+                    Icons.person_sharp,
+                    color: AppColors.appActiveColor,
+                  ),
+                  SizedBox(width: 10),
+                  Text(
+                    'Profile',
+                    style: TextStyle(color: AppColors.appActiveColor),
+                  ),
+                ],
+              ),
+              selected: _selectedIndex == 0,
+              onTap: () {
+                // Update the state of the app
+                _onItemTapped(0);
+                // Then close the drawer
+                Navigator.pop(context);
+              },
+            ),
+            const Padding(
+              padding: EdgeInsets.only(left: 20, right: 20),
+              child: MySeparator(color: AppColors.dividerColor),
+            ),
+            ListTile(
+              title: const Row(
+                children: [
+                  Icon(
+                    Icons.question_mark_rounded,
+                    color: AppColors.appActiveColor,
+                  ),
+                  SizedBox(width: 10),
+                  Text(
+                    'Developer Contact',
+                    style: TextStyle(color: AppColors.appActiveColor),
+                  ),
+                ],
+              ),
+              selected: _selectedIndex == 1,
+              onTap: () {
+                // Update the state of the app
+                _onItemTapped(1);
+                // Then close the drawer
+                Navigator.pop(context);
+              },
+            ),
+            const Padding(
+              padding: EdgeInsets.only(left: 20, right: 20),
+              child: MySeparator(color: AppColors.dividerColor),
+            ),
+            ListTile(
+              title: const Row(
+                children: [
+                  Icon(
+                    Icons.logout,
+                    color: AppColors.appActiveColor,
+                  ),
+                  SizedBox(width: 10),
+                  Text(
+                    'Logout',
+                    style: TextStyle(color: AppColors.appActiveColor),
+                  ),
+                ],
+              ),
+              selected: _selectedIndex == 2,
+              onTap: () {
+                // Update the state of the app
+                _onItemTapped(2);
+                // Then close the drawer
+                Navigator.pop(context);
+              },
+            ),
+          ],
+        ),
+      ),
+      appBar: AppBar(
+        elevation: 1,
+        backgroundColor: Colors.black,
+        foregroundColor: Colors.white,
+        centerTitle: true,
+        title: const Text("Home"),
+        actions: [IconButton(onPressed: () {}, icon: const Icon(Icons.search))],
+      ),
+      body: const Column(
+        children: [
+          Divider(
+            thickness: 0.7,
+            color: AppColors.dividerColor,
+          )
+        ],
+      ),
+    );
+  }
+}
